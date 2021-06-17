15f91721882357df341ef00966e8898313f0702b
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-06-16 22:02:04 -0300

    Criando e mapeando a Entidade Colecao

---
+++ src/main/java/com/jlcb/desafioprodutecbackend/model/Colecao.java
@@ -0,0 +1,36 @@
+package com.jlcb.desafioprodutecbackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.jlcb.desafioprodutecbackend.model.enums.TipoStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Colecao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "descricao")
+	private String descricao;
+	
+	@Column(name = "status")
+	private TipoStatus status;
+}
