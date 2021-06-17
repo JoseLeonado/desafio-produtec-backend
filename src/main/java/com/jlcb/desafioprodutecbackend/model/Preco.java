670ef0f31068cd1c306f6e645add0269ae226117
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-06-16 22:45:41 -0300

    Criando e mapeando a Entidade Preco

---
+++ src/main/java/com/jlcb/desafioprodutecbackend/model/Preco.java
@@ -0,0 +1,33 @@
+package com.jlcb.desafioprodutecbackend.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Preco {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "valor")
+	private BigDecimal valor;
+}
