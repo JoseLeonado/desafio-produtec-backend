94c39c94cbea34f65c47f4bcf2ea778c1dacbe9e
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-06-17 00:27:32 -0300

    Adicionando validações nos atributos do DTO de Usuário

--- src/main/java/com/jlcb/desafioprodutecbackend/api/dto/UsuarioDTO.java
+++ src/main/java/com/jlcb/desafioprodutecbackend/api/dto/UsuarioDTO.java
@@ -1,5 +1,7 @@
 package com.jlcb.desafioprodutecbackend.api.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +14,14 @@ import lombok.NoArgsConstructor;
 public class UsuarioDTO {
 	
 	private Long id;
+	
+	@NotBlank(message = "Informe o nome do usuário.")
 	private String nome;
+	
+	@NotBlank(message = "Informe o email do usuário.")
 	private String email;
+	
+	@NotBlank(message = "Informe a senha do usuário.")
 	private String senha;
 	private Long idEmpresa;
 //	private String status;
