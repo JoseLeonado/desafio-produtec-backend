e1e614607da89a474daf710b58f22844143b4b9d
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-06-16 21:41:39 -0300

    Initial commit

---
+++ src/main/java/com/jlcb/desafioprodutecbackend/DesafioProdutecBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jlcb.desafioprodutecbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProdutecBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProdutecBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlcb/desafioprodutecbackend/DesafioProdutecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlcb.desafioprodutecbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProdutecBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
