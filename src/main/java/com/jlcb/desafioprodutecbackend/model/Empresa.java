5095b35b1180e1bcff17df41efd0c9dc0dfa03f3
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-06-16 21:47:55 -0300

    Criando e mapeando a Entidade Empresa

---
+++ src/main/java/com/jlcb/desafioprodutecbackend/model/Empresa.java
@@ -0,0 +1,37 @@
+package com.jlcb.desafioprodutecbackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Empresa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "cnpj")
+	private String cnpj;
+	
+	@Column(name = "razao_social")
+	private String razaoSocial;
+	
+	@Column(name = "nome_fantasia")
+	private String nomeFantasia;
+	
+	@Column(name = "telefone")
+	private String telefone;
+}
