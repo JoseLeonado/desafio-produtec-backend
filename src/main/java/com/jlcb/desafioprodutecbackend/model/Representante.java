84e297d50764f56f1b65de2be1d568f105e3822b
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2021-06-16 22:50:56 -0300

    Adicionando o atributo nome fantasia na entidade Representante

--- src/main/java/com/jlcb/desafioprodutecbackend/model/Representante.java
+++ src/main/java/com/jlcb/desafioprodutecbackend/model/Representante.java
@@ -2,9 +2,13 @@ package com.jlcb.desafioprodutecbackend.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
-public class Representante extends Empresa {
+public class Representante extends Usuario {
+	
+	@Column(name = "nome_fantasia")
+	private String nomeFantasia;
 
 	@Column(name = "telefone")
 	private String telefone;
